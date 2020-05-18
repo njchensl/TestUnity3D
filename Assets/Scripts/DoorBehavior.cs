84ffcd997bee160737a3af3527a82200a7e63a1b
njchensl <nanjingchj@gmail.com>
2020-05-18 12:36:00 -0400

    doors

---
+++ Assets/Scripts/DoorBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    private GameObject m_Door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Door = transform.parent.gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // open door
+        if (other.CompareTag("Player"))
+        {
+            m_Door.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // close door
+        if (other.CompareTag("Player"))
+        {
+            m_Door.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
